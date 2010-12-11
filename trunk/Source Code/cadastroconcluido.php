adf21456333b934277b3fd9877384ea1e791adfe
biaeccentric <biaeccentric@736278b1-2dab-58cf-5e10-efcd26edaf78>
2010-12-11 06:38:49 +0000

    atualização do cadastro

--- trunk/Source Code/cadastroconcluido.php
+++ trunk/Source Code/cadastroconcluido.php
@@ -1,3 +1,18 @@
+<<<<<<< .mine
+<?php
+
+//Código que mostra que o cadastro está concluido
+$nascimento = explode('/', $_POST['nascimento']);
+
+$query = 'INSERT INTO clientes (nome, email, senha, cpf, nascimento, cep, endereco, numero, complemento, bairro, cidade, estado) VALUES (\'' . $_POST['nome'] . '\', \'' . $_POST['email'] . '\', \'' . $_POST['senha'] . '\', \'' . $_POST['cpf'] . '\', \'' . $nascimento[2] . '-' . $nascimento[1] . '-' . $nascimento[0] . '\', \'' . $_POST['cep'] . '\', \'' . $_POST['endereco'] . '\', \'' . $_POST['numero'] . '\', \'' . $_POST['complemento'] . '\', \'' . $_POST['bairro'] . '\', \'' . $_POST['cidade'] . '\', ' . $_POST['estado'] . ');';
+$resultado = mysqli_query($conexao, $query);
+
+if($resultado)
+    echo 'Cadastro conclu&iacute;do com sucesso!<br /><a href=".">Clique aqui</a> para voltar &agrave; p&aacute;gina inicial ou fa&ccedil;a o login agora na parte superior do site.';
+else
+    echo 'Erro nos dados. Tente novamente.'
+?>
+=======
 <?php
 $nascimento = explode('/', $_POST['nascimento']);
 
@@ -9,3 +24,4 @@ if($resultado)
 else
     echo 'Erro nos dados. Tente novamente';
 ?>
+>>>>>>> .r10
